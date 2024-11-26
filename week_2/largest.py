32683d897ad749c8584feb9ce4fe0aba9e6c5d32
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2024-11-26 12:45:57 +0100

    times list v generator comprehension to see which is faster (spoiler: it's generator comprehension)

--- week_2/largest.py
+++ week_2/largest.py
@@ -1,6 +1,30 @@
-def read_numbers(filename):
+import time as t
+
+
+def read_numbers_lc(filename):
+    with open(filename) as f:
+        return [int(line) for line in f.read().split('\n')]
+
+
+def read_numbers_gc(filename):
     with open(filename) as f:
         return (int(line) for line in f.read().split('\n'))
 
-numbers = read_numbers('numbers.txt')
+duration_unit = 'μs (microseconds)'
+multiplier = 1_000_000
+
+start_time = t.time()
+numbers = read_numbers_lc('numbers.txt')
+lc_duration = (t.time() - start_time) * multiplier
+
+start_time = t.time()
+numbers = read_numbers_gc('numbers.txt')
+gc_duration = (t.time() - start_time) * multiplier
+
+print(f'duration with list comprehension: {lc_duration} {duration_unit}',
+      f'duration with generator comprehension: {gc_duration} {duration_unit}',
+      f'difference {lc_duration - gc_duration} {duration_unit}',
+      f'list comprehension took longer than generator comprehension? {lc_duration > gc_duration}',
+      sep='\n', )
+
 print(max(numbers))
