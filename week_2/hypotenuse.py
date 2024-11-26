7478b4ae35937a2a5db446c418f8b3a6414bdf7d
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2024-11-26 11:33:32 +0100

    Revert "adds code from previous week (whoops! forgot)"
    This reverts commit 0cec1f8188f0a21f58eb0624b48a09aa55751c1f.

--- week_2/hypotenuse.py
+++
@@ -1,8 +0,0 @@
-from math import sqrt
-
-def hypotenuse(a: float, b: float):
-    return sqrt(a**2 + b**2)
-
-print(hypotenuse(3,4)) # 5.0
-print(hypotenuse(5,12)) # 13.0
-print(hypotenuse(1,1)) # 1.4142135623730951
--- week_2/tiny_xl.py
+++
@@ -1,9 +0,0 @@
-# can use following libs:
-# os
-
-# kiss:
-# hardcoded csv url
-# import csv
-
-
-csv_location = './data/'
